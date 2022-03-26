b94f1818f368d84d1ecccff2878ad28af8302b49
NewmarLucas <newmarlucas@gmail.com>
2022-03-26 10:52:07 -0400

    feat: add leitor de pdf

--- src/views/Music.js
+++ src/views/Music.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { Header } from '../components'
+import PDFReader from 'rn-pdf-reader-js'
 
 const Music = ({ route }) => {
   const { musicName } = route.params
@@ -10,6 +11,10 @@ const Music = ({ route }) => {
       <Header text={musicName} showBackButton />
 
       <View style={styles.pdf}>
+        <PDFReader
+          source={{ uri: 'https://storage.googleapis.com/sim-mobi.appspot.com/driverPersonalAttachments/61f9abfa5006d9001d8eacbd/criminalRecord1643752609429.criminalRecord' }}
+          style={styles.thumbnail}
+        />
       </View>
     </View>
   )
@@ -21,18 +26,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#383838'
   },
-  textLabel: {
-    marginTop: 10,
-    marginBottom: 20,
-    fontFamily: 'InterLight',
-    fontSize: 16,
-    color: '#fff'
-  },
   pdf: {
-    marginTop: '5%',
-    width: '90%',
+    marginTop: 5,
+    width: '95%',
     height: '70%',
     backgroundColor: '#fff'
+  },
+  thumbnail: {
+    width: '100%',
+    height: '100%',
   }
 })
 
