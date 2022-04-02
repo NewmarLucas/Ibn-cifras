be0c54a78553bf0ce0d7612d006ed44237467834
NewmarLucas <newmarlucas@gmail.com>
2022-04-02 10:56:33 -0400

    chore: trocando pdf viewer para usar webview

--- src/views/Music.jsx
+++ src/views/Music.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Header } from '../components'
-import PDFReader from 'rn-pdf-reader-js'
+import Constants from 'expo-constants'
+import { WebView } from 'react-native-webview'
 
 const Music = ({ route }) => {
-  const { musicName } = route.params
+  // const { musicLink } = route.params
 
   return (
     <View style={styles.container}>
-      <Header text={musicName} showBackButton />
-
       <View style={styles.pdf}>
-        <PDFReader
-          source={{ uri: 'https://storage.googleapis.com/sim-mobi.appspot.com/driverPersonalAttachments/61f9abfa5006d9001d8eacbd/criminalRecord1643752609429.criminalRecord' }}
-          style={styles.thumbnail}
+        <WebView
+          scalesPageToFit={false}
+          injectedJavaScript={`
+            function injectRules(){
+              const menu = document.getElementById("side-menu")
+              menu.parentNode.removeChild(menu)
+            }
+            injectRules()
+            `}
+          userAgent='Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+          style={styles.webViewContainertainer}
+          source={{
+            uri: 'https://www.cifraclub.com.br/ministerio-morada/so-tu-s-santo/imprimir.html',
+          }}
         />
       </View>
     </View>
@@ -24,18 +33,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#383838'
+    backgroundColor: '#383838',
+  },
+  webViewContainer: {
+    width: '100%',
+    height: '100%',
   },
   pdf: {
-    marginTop: 5,
-    width: '95%',
-    height: '70%',
-    backgroundColor: '#fff'
+    marginTop: Constants.statusBarHeight,
+    width: '98%',
+    height: '100%',
+    backgroundColor: '#fff',
   },
   thumbnail: {
     width: '100%',
     height: '100%',
-  }
+  },
 })
 
 export default Music
