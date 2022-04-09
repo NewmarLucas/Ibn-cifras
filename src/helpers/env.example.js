382e56b9d6c75b5f4b92437ff5e04650d05437c7
NewmarLucas <newmarlucas@gmail.com>
2022-04-09 13:36:21 -0300

    fix: add dados sensíveis ao env

---
+++ src/helpers/env.example.js
@@ -0,0 +1,14 @@
+const getEnvironment = () => {
+  return {
+    ENV: '',
+    FIREBASE_API_KEY: '',
+    FIREBASE_AUTH_DOMAIN: '',
+    FIREBASE_PROJECT_ID: '',
+    FIREBASE_STORAGE_BUCKET: '',
+    FIREBASE_MESSAGING_SENDER_ID: '',
+    FIREBASE_APP_ID: '',
+    FIREBASE_MEASUREMENT_ID: ''
+  }
+}
+
+export { getEnvironment }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,29 @@
-import { initializeApp } from 'firebase/app'
+import { getEnvironment } from '../helpers/env'
+import firebase, { initializeApp } from 'firebase/app'
 import 'firebase/database'
 import 'firebase/auth'
 import 'firebase/storage'
 
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} = getEnvironment()
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyD2GxcGsMSCkxwejhFNCUP2gKiHruZXGas',
-  authDomain: 'ibn-cifras.firebaseapp.com',
-  projectId: 'ibn-cifras',
-  storageBucket: 'ibn-cifras.appspot.com',
-  messagingSenderId: '98444621291',
-  appId: '1:98444621291:web:309166e5257a4280b1d5ae',
-  measurementId: 'G-M4FNHB7ZWF'
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 }
 
-if (!firebase.apps.length) initializeApp(firebaseConfig)
+initializeApp(firebaseConfig)
 
 export default firebase
