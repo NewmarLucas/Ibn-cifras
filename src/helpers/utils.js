96b42c217ef0b871a4ffaaab5b2fc540ce77e215
NewmarLucas <newmarlucas@gmail.com>
2022-07-23 10:29:08 -0400

    fix: adjusting music link

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,21 +1,39 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
+const removeMobileLinkSplited = (splitedLink) => {
+  if (splitedLink?.includes('m.cifraclub.com.br')) {
+    return splitedLink[splitedLink.indexOf('m.cifraclub.com.br')] = 'cifraclub.com.br'
+  }
+
+  return splitedLink
+}
+
+const removeMobileLinkString = (link) => {
+  if (link?.includes('m.cifraclub.com.br')) {
+    return link?.replace('m.cifraclub.com.br', 'cifraclub.com.br')
+  }
+
+  return link
+}
+
 export const parseLink = (link) => {
   const splitedLink = link?.split('/')
   const lastIndex = splitedLink.pop()
 
   if (lastIndex.includes('.html')) {
+    const newSplitedLink = removeMobileLinkSplited(splitedLink)
     const newLink = [
-      ...splitedLink,
+      ...newSplitedLink,
       lastIndex.split('.')[0],
       'imprimir.html',
     ]
     return newLink.join('/')
   } else {
-    const lastCharacter = link.split('').pop()
+    const newLink = removeMobileLinkString(link)
+    const lastCharacter = newLink.split('').pop()
     return lastCharacter === '/'
-      ? link + 'imprimir.html'
-      : link + '/imprimir.html'
+      ? newLink + 'imprimir.html'
+      : newLink + '/imprimir.html'
   }
 }
 
